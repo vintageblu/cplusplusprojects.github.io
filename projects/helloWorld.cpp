fc406d2da9da651f6b3ef9135983d547af802bc5
vintageblu <94313833+vintageblu@users.noreply.github.com>
2023-05-17 09:23:19 -0400

    Create helloWorld.cpp

---
+++ projects/helloWorld.cpp
@@ -0,0 +1,11 @@
+// Main function of the C++ program.
+/*Name:Bariat Shuaib***/
+/*Date: 18th January 2023 ***/
+/*CSC 135 01**/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+}
